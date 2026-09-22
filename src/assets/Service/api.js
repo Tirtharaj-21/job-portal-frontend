f72c36412bc5d6534fb088242df3916411f8c064
Tirtharaj Goswami <tirtharajofficial21@gmail.com>
2026-09-23 01:19:36 +0530

    fix origin

--- src/assets/Service/api.js
+++ src/assets/Service/api.js
@@ -1,20 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_url= "https://job-portal-backend-api-p9rn.onrender.com";
+// const API_url= "https://job-portal-backend-api-p9rn.onrender.com";
+const API_url = import.meta.env.VITE_API_URL;
+console.log(API_url);
 
-export const savepost = async(payload) =>{
-  try{
-  return await axios.post(`${API_url}/post`,payload);
-  }
-  catch(error){
+export const savepost = async (payload) => {
+  try {
+    console.log(API_url);
+    return await axios.post(`${API_url}/post`, payload);
+  } catch (error) {
     console.log("Error:", error.message);
-  } 
-}
-export const getAllposts = async () =>{
-  try{
+  }
+};
+export const getAllposts = async () => {
+  try {
+    console.log(API_url);
     return await axios.get(`${API_url}/posts`);
-    }
-    catch(error){
-      console.log("Error:", error.message);
-    } 
-}
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+};
